db1c61717f8e49390e6ca69cc1c266896044374c
Vanessa Cattabiani <vanebrunocattabiani@gmail.com>
2020-10-28 19:03:43 +0100

    complete

--- Artists/script/artist.js
+++ Artists/script/artist.js
@@ -13,3 +13,16 @@ function myFunction() {
   }
 }
  */
+
+const show = (section, color) => {
+  let i, tabcontent;
+  tabcontent = document.getElementsByClassName("show");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  document.getElementById(section).style.display = "block";
+  //elmnt.style.backgroundColor = color;
+};
+
+document.getElementById("defaultopen").click();
